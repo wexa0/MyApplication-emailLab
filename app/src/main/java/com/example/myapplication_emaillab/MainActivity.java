8bdbbf57d4fd860c26e37cc96807a10ed222b4a2
wexa0 <443200416@student.ksu.edu.sa>
2024-04-20 17:47:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication_emaillab/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.myapplication_emaillab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    // define objects for edit text and button
+    Button button;
+    EditText sendto, subject, body;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Getting instance of edittext and button
+        sendto = findViewById(R.id.editText1);
+        subject = findViewById(R.id.editText2);
+        body = findViewById(R.id.editText3);
+        button = findViewById(R.id.button);
+
+        // attach setOnClickListener to button with Intent object define in it
+        button.setOnClickListener(view -> {
+            String emailsend = sendto.getText().toString();
+            String emailsubject = subject.getText().toString();
+            String emailbody = body.getText().toString();
+
+            // define Intent object with action attribute as ACTION_SEND
+            Intent intent = new Intent(Intent.ACTION_SEND);
+
+            // add three fields to intent using putExtra function
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{emailsend});
+            intent.putExtra(Intent.EXTRA_SUBJECT, emailsubject);
+            intent.putExtra(Intent.EXTRA_TEXT, emailbody);
+
+            // set type of intent
+            intent.setType("message/rfc822");
+
+            // startActivity with intent with chooser as Email client using createChooser function
+            startActivity(Intent.createChooser(intent, "Choose an Email client :"));
+        });
+    }
+}
